c8fc2db2cdb141c8d82d6ac3c22eef1858b96ac2
billlzzz10 <billlzzz10@gmail.com>
2025-09-28 16:56:01 +0700

    Update vite.config.cjs

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,1 +1,9 @@
 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  // ใส่ชื่อรีโปคุณแทน task-flow
+  base: "/task-flow/",
+});
